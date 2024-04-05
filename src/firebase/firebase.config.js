472fd914ae61ad42eeefc7fb3f8330396f67f6d7
adilhamza <adilhamza486@gmail.com>
2024-04-05 23:58:12 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app"; 
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD_IVn2O4ZT2jnM4HjQnpTmygVtukPlato",
+  authDomain: "dragon-news-45cc2.firebaseapp.com",
+  projectId: "dragon-news-45cc2",
+  storageBucket: "dragon-news-45cc2.appspot.com",
+  messagingSenderId: "992359333754",
+  appId: "1:992359333754:web:cfb751bccdd21a247b20c3"
+};
+
+
+const app = initializeApp(firebaseConfig);
+export default app;
